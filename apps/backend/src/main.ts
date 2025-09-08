ac32d597ff443e1e33009f0402be745ce5836a6f
Ceyhun Carpar <ceyhuncarpar@gmail.com>
2025-09-08 16:07:16 +0300

    feat(backend): enable cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,6 +5,7 @@ import 'reflect-metadata'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true // Trim non listed data by any validator
